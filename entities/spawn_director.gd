d4fed47630a1c9ef967c0461f8eb14bbf7ed5ce7
LorenzoDezi <dezio95.ld@gmail.com>
2026-09-20 09:51:43 +0200

    feat: basic reverse time mechanic for testing

--- entities/spawn_director.gd
+++ entities/spawn_director.gd
@@ -1,11 +1,18 @@
 class_name SpawnDirector
 extends Node2D
 
-@export var reversed = false
+var reversed = false
 
 @onready var spawners: Array = get_children()
+
+func _input(event):
+	if event.is_action_pressed("reverse_time"):
+		reversed = true
+	elif event.is_action_released("reverse_time"):
+		reversed = false
 	
 func _process(delta: float) -> void:
 	if reversed: delta = -delta
+	
 	for spawner in spawners:
 		spawner.update(delta)
