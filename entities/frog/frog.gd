380e90ba61149bf16191ecf4ba39f9f60b23556c
LorenzoDezi <dezio95.ld@gmail.com>
2026-09-26 14:19:38 +0200

    feat: carrying obstacles

--- entities/frog/frog.gd
+++ entities/frog/frog.gd
@@ -9,6 +9,10 @@ extends Area2D
 @onready var animated_sprite: AnimatedSprite2D = $CollisionShape2D/AnimatedSprite2D
 @onready var preventive_raycast: RayCast2D = $CollisionShape2D/PreventiveRaycast
 
+var is_being_carried = false
+var carrying_obstacle: Obstacle
+var last_carrying_obstacle_pos: Vector2
+
 var is_performing_step = false
 var curr_step_time = 0.0
 var start_position: Vector2
@@ -20,6 +24,7 @@ func _ready() -> void:
 	start_position = curr_position
 	animated_sprite.play("Idle")
 	area_entered.connect(_on_area_entered)
+	area_exited.connect(_on_area_exited)
 
 func _physics_process(delta: float) -> void:
 	_process_movement(delta)
@@ -28,18 +33,22 @@ func _on_area_entered(area: Area2D) -> void:
 	if area is Obstacle:
 		var obstacle = area as Obstacle
 		if obstacle.dangerous:
-			var dead_frog = dead_frog_scene.instantiate()
-			get_parent().add_child(dead_frog)
-			dead_frog.position = position
-			dead_frog.rotation = rotation
-			if is_performing_step:
-				_end_step()
-			position = start_position
-			frog_shape.rotation = Vector2.RIGHT.angle()
-			#TODO: reset input or a small delay
+			_die()
+		elif obstacle.carrying:
+			carrying_obstacle = obstacle
+			last_carrying_obstacle_pos = obstacle.position
+			if !is_performing_step:
+				is_being_carried = true
 	else:
 		#TODO
 		pass
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is Obstacle:
+		var obstacle = area as Obstacle
+		if obstacle.carrying && obstacle == carrying_obstacle:
+			carrying_obstacle = null
+			is_being_carried = false
 
 func _process_movement(delta: float) -> void:
 	if is_performing_step:
@@ -50,6 +59,20 @@ func _process_movement(delta: float) -> void:
 				next_position, curr_step_time / time_to_perform_step
 		)
 	else:
+		if is_being_carried:
+			var carry_position: Vector2 = position + (carrying_obstacle.position - last_carrying_obstacle_pos)
+			var og_raycast_rotation = preventive_raycast.rotation
+			preventive_raycast.rotation = Vector2.UP.angle_to(position.direction_to(carry_position))
+			preventive_raycast.force_raycast_update()
+			if preventive_raycast.is_colliding():
+				_die()
+				preventive_raycast.rotation = og_raycast_rotation
+				return
+			
+			preventive_raycast.rotation = og_raycast_rotation
+			position = carry_position
+			last_carrying_obstacle_pos = carrying_obstacle.position
+
 		var target_position: Vector2 = position
 		if Input.is_action_pressed("movement_left"):
 			target_position.x -= step
@@ -76,6 +99,7 @@ func _process_step(target_position: Vector2) -> void:
 
 func _start_step(target_position: Vector2) -> void:
 	is_performing_step = true
+	is_being_carried = false
 	curr_step_time = 0.0
 	curr_position = position
 	next_position = target_position
@@ -85,3 +109,20 @@ func _end_step() -> void:
 	is_performing_step = false
 	curr_step_time = time_to_perform_step
 	animated_sprite.play("Idle")
+	if carrying_obstacle != null:
+		last_carrying_obstacle_pos = carrying_obstacle.position
+		is_being_carried = true
+
+func _die() -> void:
+	var dead_frog = dead_frog_scene.instantiate()
+	if carrying_obstacle != null:
+		carrying_obstacle.add_child(dead_frog)
+	else:
+		get_parent().add_child(dead_frog)
+	dead_frog.global_position = global_position
+	dead_frog.global_rotation = global_rotation
+	if is_performing_step:
+		_end_step()
+	position = start_position
+	frog_shape.rotation = Vector2.RIGHT.angle()
+	#TODO: reset input or a small delay
